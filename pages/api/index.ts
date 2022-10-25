494f959ad30f82f5f286a8dec228a3aa046d5d77
Jamal Bahammou <44509703+jamal-Bahammou@users.noreply.github.com>
2022-10-25 16:42:48 +0100

    Axios baseURL error

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const Axios = axios.create({
-    baseURL: 'http://127.0.0.1:3030',
+    baseURL: process.env.SERVER_HOST,
     headers: {'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Credentials': true, 'Content-Type': 'application/json'},
 })
 

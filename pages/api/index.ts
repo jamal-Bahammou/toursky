53f0072732f7455629f83cb5a6a4b17d888aee7d
Jamal Bahammou <44509703+jamal-Bahammou@users.noreply.github.com>
2022-11-01 15:28:13 +0100

    CORS ERROR

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,10 +1,3 @@
 import axios from 'axios'
 
-export const Axios = axios.create({
-    baseURL: 'https://toursky.herokuapp.com',
-    headers: {
-        'Access-Control-Allow-Origin': 'https://toursky.herokuapp.com',
-        'Access-Control-Allow-Credentials': true,
-        'Content-Type': 'application/json'
-    },
-})
+export const Axios = axios.create({ baseURL: 'https://toursky.herokuapp.com'})
